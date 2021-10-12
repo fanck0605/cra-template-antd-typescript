121c53ebef86a35bb8055623011ab8c436bedf27
Chuck Fan <fanck0605@qq.com>
2021-10-12 15:19:14 +0800

    Less Modules in react-scripts test

--- template/craco.config.js
+++ template/craco.config.js
@@ -1,5 +1,39 @@
 const CracoLessPlugin = require('craco-less');
 
+// The following override should have been in CracoLessPlugin
+CracoLessPlugin.overrideJestConfig = ({ context, jestConfig }) => {
+  const moduleNameMapper = jestConfig.moduleNameMapper;
+  const cssModulesPattern = Object.keys(moduleNameMapper).find((p) =>
+    p.match(/\\\.module\\\.\(.*?css.*?\)/)
+  );
+
+  if (!cssModulesPattern) {
+    throw new Error(
+      `Can't find CSS Modules pattern under moduleNameMapper in the ${context.env} jest config!`
+    );
+  }
+
+  moduleNameMapper[cssModulesPattern.replace('css', 'css|less')] =
+    moduleNameMapper[cssModulesPattern];
+  delete moduleNameMapper[cssModulesPattern];
+
+  const transformIgnorePatterns = jestConfig.transformIgnorePatterns;
+  const cssModulesPatternIndex = transformIgnorePatterns.findIndex((p) =>
+    p.match(/\\\.module\\\.\(.*?css.*?\)/)
+  );
+  if (cssModulesPatternIndex === -1) {
+    throw new Error(
+      `Can't find CSS Modules pattern under transformIgnorePatterns in the ${context.env} jest config!`
+    );
+  }
+
+  transformIgnorePatterns[cssModulesPatternIndex] = transformIgnorePatterns[
+    cssModulesPatternIndex
+  ].replace('css', 'css|less');
+
+  return jestConfig;
+};
+
 module.exports = {
   babel: {
     plugins: [
